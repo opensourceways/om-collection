b1ffc39fd10f9fe29a721a6eff52b793a43f0ab9
zhongjun2 <526521735@qq.com>
2020-03-30 14:29:11 +0800

    update cmd/tools/ownercheck.go.

--- cmd/tools/ownercheck.go
+++ cmd/tools/ownercheck.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 The openeuler community Authors.
+Copyright 2019 The community Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
