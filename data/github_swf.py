e3033c0cacabface9ab37d1c1836fe8e0147a053
hwx5333735 <haoxiangyu3@huawei.com>
2020-11-24 16:42:16 +0800

    ES del,github get total

--- data/github_swf.py
+++ data/github_swf.py
@@ -21,7 +21,7 @@ import requests
 import json
 import time
 
-from datetime import datetime
+import datetime
 from data import common
 from data.common import ESClient
 from collect.github import GithubClient
@@ -36,7 +36,17 @@ class GitHubSWF(object):
         self.esClient = ESClient(config)
         self.headers = {}
         self.github_authorization = config.get('github_authorization')
-
+        self.url = config.get('es_url')
+        self.from_data = config.get("from_data")
+        self.headers = {'Content-Type': 'application/json', 'Authorization': config.get('authorization')}
+        self.github_types = config.get('github_types').split(',')
+        self.github_size = config.get('github_size')
+        self.github_field = config.get('github_field')
+        self.github_index_name = config.get('github_index_name')
+        self.gitee_types = config.get('gitee_types').split(',')
+        self.gitee_field = config.get('gitee_field')
+        self.gitee_size = config.get('gitee_size')
+        self.gitee_index_name = config.get('gitee_index_name')
 
     def run(self, from_date):
         startTime = time.time()
@@ -49,10 +59,81 @@ class GitHubSWF(object):
             actions += action
         self.esClient.safe_put_bulk(actions)
 
+        for type in self.github_types:
+            self.getTotal(type=type, index_name=self.github_index_name, field=self.github_field, size=self.github_size)
+        for type in self.gitee_types:
+            self.getTotal(type=type, index_name=self.gitee_index_name, field=self.gitee_field, size=self.gitee_size, search=',"must": [{ "match": { "is_gitee_repo":1 }}]')
+
         endTime = time.time()
         spent_time = time.strftime("%H:%M:%S", time.gmtime(endTime - startTime))
         print("Collect github star watch fork data: finished after ", spent_time)
 
+    def getTotal(self, type, index_name, url=None, date=None, field=None, size='10', search=''):
+        if not url:
+            url = self.url + '/' + index_name + '/_search'
+        if not date:
+            date = self.from_data[:4] + '-' + self.from_data[4:6] + '-' + self.from_data[6:]
+        datei = datetime.datetime.strptime(date, "%Y-%m-%d")
+        dateii = datei
+        totalmark = 'is_github_' + type + '_total'
+        while True:
+            datenow = datetime.datetime.strptime(datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d"),
+                                                 "%Y-%m-%d")
+            if dateii == datenow + datetime.timedelta(days=1):
+                break
+            dateiise = dateii
+            dateii += datetime.timedelta(days=1)
+            stime = datetime.datetime.strftime(dateiise, "%Y-%m-%d")
+            data = '''{
+          "size": 0,
+          "query": {
+            "bool": {
+              "filter": [
+                {
+                  "range": {
+                    "created_at": {
+                        "gte":"%sT00:00:00.000+0800",
+                        "lt":"%sT00:00:00.000+0800"
+                    }
+                  }
+                },
+                {
+                  "query_string": {
+                    "analyze_wildcard": true,
+                    "query": "*"
+                  }
+                }
+              ]%s
+            }
+          },
+          "aggs": {
+            "2": {
+              "terms": {
+                "field": "%s",
+                "size": %s,
+                "order": {
+                  "_key": "desc"
+                },
+                "min_doc_count": 1
+              },
+              "aggs": {
+                "3": {
+                  "max": {
+                    "field": "%s"
+                  }
+                }
+              }
+            }
+          }
+        }''' % (str(dateiise).split()[0], str(dateii).split()[0], search, field, size, type)
+            res = json.loads(requests.get(url=url, headers=self.headers, verify=False, data=data.encode('utf-8')).content)
+            num = sum([int(r['3']['value']) for r in res["aggregations"]["2"]["buckets"]])
+            body = {'created_at': stime+'T00:00:00.000+0800', totalmark: 1, 'total_num': num}
+            ID = totalmark + stime
+            data = common.getSingleAction('github_swf_mindspore_20200604_test', ID, body)
+            if num > 0:
+                print('%s:%s' % (stime, num))
+                self.esClient.safe_put_bulk(data)
 
     def getRepoNames(self):
         gitclient = GithubClient(self.org, "", self.github_authorization)
@@ -74,7 +155,7 @@ class GitHubSWF(object):
     def getSWF(self, repo):
         client = GithubClient(self.org, repo, self.github_authorization)
         r = client.repo()
-        r["swf_update_time"] = datetime.now().strftime('%Y-%m-%dT%H:%M:%S+08:00')
+        r["swf_update_time"] = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S+08:00')
         # r["swf_update_time"] = "2020-06-19T22:21:25+08:00"
         id = "swf_" + r["swf_update_time"] + r.get("full_name")
         action = common.getSingleAction(self.index_name, id, r)
