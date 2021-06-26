b3d32df330c0a747f9d3e6792556e4a401af7720
zhongjun2 <526521735@qq.com>
2021-06-26 08:58:36 +0000

    update data/git_commit.py.

--- data/git_commit.py
+++ data/git_commit.py
@@ -175,9 +175,11 @@ class GitCommit(object):
         self.esClient.safe_put_bulk(action)
         print(repo, " data has stored into ES")
 
-    def pull_Repo_To_Local(self, repourl, project, path):
+    def pull_Repo_To_Local(self, repourl, project=None, path):
         repo = repourl.split("/")[-1]
         website = repourl.split("/")[2]
+        if project is None:
+            project = repourl.split("/")[-2]
         username = base64.b64decode(self.username).decode()
         passwd = base64.b64decode(self.password).decode()
 
