36d6fb662700a6d2b4f68098ee45fa4c2d722da8
Administrator <sch_na@163.com>
2022-02-17 17:39:59 +0800

    lower internalUsers

--- data/gitee.py
+++ data/gitee.py
@@ -1426,7 +1426,7 @@ class Gitee(object):
         users = []
         for line in f.readlines():
             if line != "\n":
-                users.append(line.split('\n')[0].lower())
+                users.append(line.split('\n')[0])
         print(users)
         print(len(users))
         return users
