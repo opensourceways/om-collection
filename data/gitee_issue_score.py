3f04b84e78846fbefe4224d44f9df0d0eb607871
reus <reuscmd@163.com>
2022-03-26 16:23:46 +0800

    add issue_author for gitee_issue_score

--- data/gitee_issue_score.py
+++ data/gitee_issue_score.py
@@ -21,6 +21,7 @@ import requests
 import yaml
 
 from data import common
+from data.gitee import Gitee
 
 HEADERS = {'Content-Type': 'application/json', 'charset': 'UTF-8'}
 PAYLOAD = {}
@@ -33,10 +34,13 @@ class GiteeScore(object):
         self.access_token = config.get('access_token')
         self.index_name = config.get('index_name')
         self.repo_path = config.get('repo_path')
+        self.owner = config.get('owner')
+        self.repository = config.get('repository')
         self.issue_state = config.get('issue_state')
         self.score_admin_file_path = config.get('score_admin_file_path')
         self.esClient = common.ESClient(config)
         self.platform_name = platform.system().lower()
+        self.gitee_api = Gitee(config)
 
     @common.show_spend_seconds_of_this_function
     def run(self, from_date):
@@ -65,12 +69,14 @@ class GiteeScore(object):
 
     @common.show_spend_seconds_of_this_function
     def process_gitee_score(self):
-        issue_number_list = self.get_repo_issue_numbers(per_page=100)
-        issue_total_count = len(issue_number_list)
+        issue_brief_list = self.get_repo_issue_numbers(per_page=100)
+        issue_total_count = len(issue_brief_list)
 
         actions = ''
-        for issue_number in issue_number_list:
-            comment_index = issue_number_list.index(issue_number)
+        for issue_brief in issue_brief_list:
+            issue_number = issue_brief[0]
+            issue_author = issue_brief[1]
+            comment_index = issue_brief_list.index(issue_brief)
             issue_comment_list = self.get_comments_by_issue_number(issue_number)
             author_username, score = self.parse_comment_list(issue_comment_list)
             print(f'Succeed to parse issue: {issue_number}, \t{comment_index + 1}/{issue_total_count} '
@@ -80,6 +86,7 @@ class GiteeScore(object):
 
             content_body = {}
             content_body['issue_number'] = issue_number
+            content_body['issue_author'] = issue_author
             content_body['scoring_admin'] = author_username
             content_body['score'] = score
             action = common.getSingleAction(self.index_name, issue_number, content_body)
@@ -88,8 +95,6 @@ class GiteeScore(object):
 
     @common.show_spend_seconds_of_this_function
     def get_repo_issue_numbers(self, per_page):
-        issue_number_list = []
-
         res_data_list = []
         start_page = 1
 
@@ -112,12 +117,11 @@ class GiteeScore(object):
             res_data_list.extend(page_data_list)
             start_page += 1
 
-        for issue in res_data_list:
-            issue_number_list.append(issue.get('number'))
-
+        issue_brief_list = [(issue.get('number'), issue.get('user').get('login'))
+                            for issue in res_data_list]
         print(f'Succeed to collect issue numbers: {len(res_data_list)}')
 
-        return issue_number_list
+        return issue_brief_list
 
     def get_comments_by_issue_number(self, issue_number):
         comment_list = []
