385ba16da0b55d1c601420bad73036548b032f3d
zhongjun2 <526521735@qq.com>
2022-05-23 20:25:42 +0800

    Added license in code

--- data/activities_practice.py
+++ data/activities_practice.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 The community Authors.
+# A-Tune is licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# Create: 2022-03
+#
 import calendar
 import datetime
 import json
@@ -117,7 +131,6 @@ class ActivitiesPractice(object):
                     current_assign_user = assign_user
                     is_pass_assign = 1
                     assign_time = comment['created_at']
-                    # res['assign_time'] = comment['created_at']
 
                 if comments:
                     last_commtnt_body = comments[-1]['body']
